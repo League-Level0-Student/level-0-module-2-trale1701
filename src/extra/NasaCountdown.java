98f9417e7e2c6f5eae691db311d05b4bd7ac7465
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-28 13:24:21 -0700

    Check point 1

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,7 +3,9 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown – or the rocket won’t launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -15,11 +17,15 @@ package extra;
 public class NasaCountdown {
 	public static void main(String[] args) throws InterruptedException {
 		// 2. Use a pop-up to ask the user where to start counting from
-
+String start =JOptionPane.showInputDialog("Where do you want tot start?");
 		// 3. Change the countdown to use the new starting point
-		
+		int startNum = Integer.parseInt(start);
 		// 1. Print a countdown from 10 to 0 on the console
+	for (int i = startNum; i >= 0 ; i=i-1) {
+		System.out.println(i);
 	
+		
+	}
 			// 4. Use the speak method to hear the countdown.
 		
 			// 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -18,8 +18,12 @@ AnimalFarm() {
 	 * 1. Ask the user which animal they want, then play the sound of that
 	 * animal.
 	 */
-
+String animal = JOptionPane.showInputDialog("What is your favorite animal?");
 	/* 2. Make it so that the user can keep entering new animals. */
+if(animal.equalsIgnoreCase("cow")) {
+	playMoo();
+}
+
 }
 
 void playMoo() {
